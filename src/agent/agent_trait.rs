4c329182768df3f7799f51c13da6a79023e50ca2
thepyper <thepyper@users.noreply.github.com>
2025-09-17 11:45:21 +0200

    fix: add lifetime parameter to Agent trait

--- src/agent/agent_trait.rs
+++ src/agent/agent_trait.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait Agent {
+pub trait Agent<'a> {
     fn name(&self) -> &str;
     async fn execute(&self, input: &str) -> Result<String>;
 }

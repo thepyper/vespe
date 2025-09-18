374bcfe95e0ac946206825f323a6c3b9fa206937
thepyper <thepyper@users.noreply.github.com>
2025-09-18 10:20:23 +0200

    feat: Complete Phase 4 - Adapt agent logic to new parsing system

--- src/llm/query_formatter.rs
+++ src/llm/query_formatter.rs
@@ -6,17 +6,17 @@ pub fn format_messages(messages: &[Message]) -> String {
     messages
         .iter()
         .map(|m| match m {
-            Message::System(s) => format!("System: {}\n", s),
-            Message::User(s) => format!("User: {}\n", s),
+            Message::System(s) => format!("System: {{}}\n", s),
+            Message::User(s) => format!("User: {{}}\n", s),
             Message::Assistant(contents) => {
                 let content_str = contents
                     .iter()
                     .map(|c| format!("{:?}", c))
                     .collect::<Vec<_>>()
                     .join(" ");
-                format!("Assistant: {}\n", content_str)
+                format!("Assistant: {{}}\n", content_str)
             }
-            Message::Tool(output) => format!("Tool Output: {:?}\n", output),
+            Message::Tool(output) => format!("Tool Output: {{:?}}\n", output),
         })
         .collect::<Vec<_>>()
         .join("\n")

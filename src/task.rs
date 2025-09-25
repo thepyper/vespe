feb0830a56353495a305d5dfb4a8134be07ac23e
thepyper <thepyper@users.noreply.github.com>
2025-09-25 23:22:15 +0200

    Fix: Unclosed delimiter in TaskState impl block

--- src/task.rs
+++ src/task.rs
@@ -34,6 +34,7 @@ impl TaskState {
             TaskState::Working => matches!(next_state, TaskState::Completed | TaskState::Error | TaskState::Failed),
             TaskState::Error => matches!(next_state, TaskState::Failed | TaskState::Error), // From Error, can transition to Failed or stay in Error
             TaskState::Failed | TaskState::Completed => false, // Final states, no transitions out
+        }
     }
 }
 

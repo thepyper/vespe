0e1acb0953cce830e0256750a273daad55a1d55e
thepyper <thepyper@users.noreply.github.com>
2025-10-01 14:06:09 +0200

    initial md things

---
+++ src/bin/test_markdown.rs
@@ -0,0 +1,33 @@
+use markdown::{to_mdast, ParseOptions};
+use markdown::message::Message;
+
+fn main()  -> Result<(), Message>  {
+     
+    let examples = vec![
+r###"# Hi *Earth*!"###,
+r###"
+# Hi *Earth*!!!
+This is line 2
+
+This is line 4
+
+This is line 6
+"###,        
+r###"
+# Objective <!-- my note -->
+
+# HeyHey
+
+haha haha haha
+
+"###
+    ];
+    
+    for (i, example) in examples.into_iter().enumerate() {
+        let tree = to_mdast(example, &ParseOptions::default())?;
+        println!("example {i}:\n{:?}", tree);        
+        println!("string  {i}:\n{}", tree.to_string());
+    }
+    
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,42 @@ pub use error::*;
 pub use utils::*;
 pub use memory::*;
 */
+
+use anyhow::Result;
+
+pub enum TaskStatePlanSectionItem{
+    LocalTask(String),
+    ReferencedTask(String),
+}
+
+pub enum TaskStateSection {
+    Intent{ title: String, text: String },
+    Plan{ title: String, items: Vec<TaskStatePlanSectionItem> },
+    Text{ title: String, items: String }, 
+}
+
+pub struct TaskState {
+    /// Original markdown file 
+    md: String,
+    /// Original markdown file parsed ast
+    mdast: markdown::mdast::Node,
+    /// State structure parsed
+    sections: Vec<TaskStateSection>,
+}
+
+pub struct TaskMetadata {
+    name: String,
+}
+
+pub struct Task {
+    uid: String,
+    meta: TaskMetadata,
+    state: TaskState,
+}
+
+impl TaskState {
+    
+    fn from_md(md : &str) -> Result<TaskState> {
+        unimplemented!();
+    }        
+}

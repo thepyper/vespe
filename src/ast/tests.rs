6dc27ec79985e01d0a25254367470aae636e9d9b
thepyper <thepyper@users.noreply.github.com>
2025-10-15 01:45:06 +0200

    Fix: Update src/ast/tests.rs with correct #[path] attributes

--- src/ast/tests.rs
+++ src/ast/tests.rs
@@ -1,11 +1,22 @@
+#[path = "tests/test_batch_1.rs"]
 mod test_batch_1;
+#[path = "tests/test_batch_2.rs"]
 mod test_batch_2;
+#[path = "tests/test_batch_3.rs"]
 mod test_batch_3;
+#[path = "tests/test_batch_4.rs"]
 mod test_batch_4;
+#[path = "tests/test_batch_5.rs"]
 mod test_batch_5;
+#[path = "tests/test_batch_6.rs"]
 mod test_batch_6;
+#[path = "tests/test_batch_7.rs"]
 mod test_batch_7;
+#[path = "tests/test_batch_8.rs"]
 mod test_batch_8;
+#[path = "tests/test_batch_9.rs"]
 mod test_batch_9;
+#[path = "tests/test_batch_10.rs"]
 mod test_batch_10;
+#[path = "tests/test_batch_11.rs"]
 mod test_batch_11;

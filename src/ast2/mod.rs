44361cd6f44ca584e9dc452b6c3f9e8b3d1818cc
thepyper <thepyper@users.noreply.github.com>
2025-10-16 01:02:15 +0200

    Handwritten ast

---
+++ src/ast2/mod.rs
@@ -0,0 +1,305 @@
+
+struct Position {
+    offset: usize,      /// 0-based character offset
+    line: usize,        /// 1-based line
+    column: usize,      /// 1-based column
+}
+
+struct Range {
+    begin: Position,
+    end: Position,
+}
+
+struct Text {
+    range: Range,
+}
+
+enum CommandKind {
+    Tag,        // for debug purpose
+    Include,
+    Inline,
+    Answer,
+    Summarize,
+    Derive,
+    Repeat,
+}
+
+struct Command {
+    kind: CommandKind,
+    range: Range,
+}
+
+struct Parameters {
+    parameters: serde_json::Value,
+    range: Range,
+}
+
+struct Argument {
+    range: Range,
+}
+
+struct Arguments {
+    arguments: Vec<Argument>,
+    range: Range,
+}
+
+struct Tag {
+    command: Command,
+    parameters: Parameters,
+    arguments: Arguments,
+    range: Range,   
+}
+
+enum AnchorKind 
+{
+    Begin,
+    End,
+}
+
+struct Anchor {
+    command: Command,
+    uuid: Uuid,
+    kind: AnchorKind,
+    parameters: Parameters,
+    arguments: Arguments,
+    range: Range,
+}
+
+enum Content {
+    Text(Text),
+    Tag(Tag),
+    Anchor(Anchor),
+}
+
+struct Document {
+    content: Vec<Content>,
+    range: Range,
+}
+
+pub struct Parser<'a> {
+    document: &'a str,
+    position: Position,
+    iterator: Chars<'a>,
+}
+
+pub struct ParserStatus<'a> {
+    position: Position,
+    iterator: Chars<'a>,
+}
+
+impl <'a> Parser<'a> {
+    pub fn new(document: &'a str) -> Self {
+        Self {
+            document,
+            position: Position { offset: 0, line: 1, column: 1 },
+            iterator: document.chars(),
+        }
+    }
+    pub fn get_position(&self) -> Position {
+        self.position.clone()
+    }
+    pub fn get_offset(&self) -> usize {
+        self.position.offset
+    }
+    pub fn remain(&self) -> &'a str {
+        self.iterator.as_str()
+    }
+    pub fn is_eod(&self) -> bool {
+        self.remain().is_empty()
+    }
+    pub fn is_begin_of_line(&self) -> bool {
+        self.position.column == 1
+    }
+    pub fn consume(&mut self, xs: &str) -> bool {
+        for x in xs.chars() {
+            match self.advance() {
+                None => {
+                    return false;
+                }
+                Some(y) if y != x {
+                    return false;
+                }
+                Some(y) {
+                    continue;
+                }
+            }
+        }
+    }    
+    pub fn skip_one_of(&mut self, xs: &str) -> bool {
+        let status = self.store();
+        for x in xs.chars() {
+            match self.advance() {
+                None => {
+                    return false;
+                }
+                Some(y) if y == x {
+                    return true;
+                }
+                Some(y) {
+                    self.load(status);
+                }
+            }
+        }
+        self.load(status);
+        false
+    }
+    pub fn skip_many_of(&mut self, xs: &str) {
+        while self.skip_one_of(xs) {}
+    }
+    pub fn skip_many_whitespaces(&mut self) {
+        self.skip_many_of(" \t\r");
+    }
+    pub fn advance(&mut self) -> Option<char> {
+        match self.iterator.next() {
+            None => None,
+            Some(c) => {
+                self.position.offset += 1;
+                match c {
+                    c if c = '\n' => {
+                        self.position.line += 1;
+                        self.position.column = 1;
+                    }
+                    _ => {
+                        self.position.column += 1;
+                    }
+                }
+                Some(c)                
+            }
+        }
+    }
+    pub fn store(&self) -> ParserStatus {
+        ParserStatus {
+            position: self.position.clone(),
+            iterator: self.iterator.clone(),
+        }
+    }
+    pub fn load(&mut self, status: ParserStatus) {
+        self.position = status.position;
+        self.iterator = status.iterator;
+    }
+}
+
+fn parse_document(document: &str) -> Result<Document> {
+
+    let mut parser = Parser::new(document);
+    let begin = parser.get_position();
+    let content = parse_content(document, &mut parser)?;
+    let end   = parser.get_position();
+
+    Document {
+        content: content,
+        range: Range { begin, end },
+    }
+}
+
+fn parse_content(document: &str, parser: &mut Parser) -> Result<Vec<Content>> {
+
+    let mut contents = Vec::new();
+
+    while !parser.is_eod() {
+        if let Some(tag) = try_parse_tag(document, parser)? {
+            contents.push(Tag(tag));            
+        } else if let Some(anchor) = try_parse_anchor(document, parser)? {
+            contents.push(Anchor(anchor));
+        } else if let Some(text) = try_parse_text(document, parser)? {
+            contents.push(Text(text));
+        } else {
+            // TODO parse error
+        }
+    }
+
+    Ok(contents)
+}
+
+fn try_parse_tag(document: &str, parser: &mut Parser) -> Result<Option<Tag>> {
+
+    let status = parser.store();
+
+    match _try_parse_tag(document, parser)? {
+        None => {
+            parser.load(status);
+            None           
+        }
+        Some(x) => Some(x)
+    }    
+} 
+
+fn _try_parse_tag(document: &str, parser: &mut Parser) -> Result<Option<Tag>> {
+
+    let begin = parser.get_position();
+
+    let command = _try_parse_command(document, parser)?;
+    if command.is_none() {
+        return Ok(None);
+    }
+
+    parser.skip_many_whitespaces();
+
+    let parameters = _try_parse_parameters(document, parser)?;
+    
+    parser.skip_many_whitespaces();
+
+    let arguments = _try_parse_arguments(document, parser)?;
+
+    let end = parser.get_position();
+
+    parser.skip_many_whitespaces();
+
+    if !parser.consume("\n") {
+        // TODO errore, text dopo arguments e prima di fine linea!?
+    }
+
+    Ok(Some(Tag {
+        command,
+        parameters,
+        arguments,
+        range: Range {
+            begin, end 
+        }
+    }))
+}
+
+fn _try_parse_command(document: &str, parser: &mut Parser) -> Result<Option<Command>> {
+
+    let begin = parser.get_position();
+
+    if !parser.consume("@") {
+        return Ok(None);
+    }
+
+    let tags_list = vec![
+        ("tag", CommandKind::Tag),
+        ("include", CommandKind::Include),
+        ("inline", CommandKind::Inline),
+        ("answer", CommandKind::Answer),
+        ("summarize", CommandKind::Summarize),
+        ("derive", CommandKind::Derive),
+        ("repeat", CommandKind::Repeat),
+    ];
+
+    let mut kind = None;
+
+    let status = parser.store();
+    for (try_name, try_kind) in tags_list {
+        if !parser.consume(try_name) {
+            parser.load(status);
+        } else {
+            kind = try_kind;
+            break;
+        }
+    }
+
+    if kind.is_none() {
+        return Ok(None); // unknown tag, treat as normal text
+    }
+
+    let end = parser.get_position();
+
+    Ok(Some(Command {
+        kind, 
+        range: Range {
+            begin,
+            end,
+        }
+    }))
+}

5dd56ab07d5394ac7e93002ba7f10b773167ccea
thepyper <thepyper@users.noreply.github.com>
2025-10-16 13:55:51 +0200

    Fix: ast2/mod.rs document not found

--- src/ast2/mod.rs
+++ src/ast2/mod.rs
@@ -740,7 +740,7 @@ fn _try_parse_nude_value(parser: &mut Parser) -> Result<Option<serde_json::Value
     if let Ok(Some(x)) = _try_parse_nude_float(parser) {
         return Ok(Some(json!(x)));
     }
-    parser.load(document);
+    parser.load(input);
 
     if let Ok(Some(x)) = _try_parse_nude_bool(parser) {
         return Ok(Some(json!(x)));

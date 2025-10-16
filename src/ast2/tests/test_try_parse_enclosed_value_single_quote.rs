b562bc1bce29a88147ab259ed0d0f48ae140aa44
thepyper <thepyper@users.noreply.github.com>
2025-10-16 17:10:05 +0200

    fixes

--- src/ast2/tests/test_try_parse_enclosed_value_single_quote.rs
+++ src/ast2/tests/test_try_parse_enclosed_value_single_quote.rs
@@ -6,8 +6,8 @@ use serde_json::json;
 #[test]
 fn test_try_parse_enclosed_value_single_quote() -> Result<()> {
     let mut parser = Parser::new("'hello world'" );
-    let value = _try_parse_enclosed_value(&mut parser, "'")?;
-    assert_eq!(value, Some(json!("hello world")));
+    let value = _try_parse_enclosed_value(&mut parser, '\'')?;
+    assert_eq!(value, Some("hello world".to_string()));
     assert_eq!(parser.get_position(), create_pos(13, 1, 14));
     Ok(())
 }
--- src/ast2/tests/test_try_parse_enclosed_value_unterminated.rs
+++ src/ast2/tests/test_try_parse_enclosed_value_unterminated.rs
@@ -5,7 +5,7 @@ use anyhow::Result;
 #[test]
 fn test_try_parse_enclosed_value_unterminated() -> Result<()> {
     let mut parser = Parser::new("\"hello");
-    let result = _try_parse_enclosed_value(&mut parser, "\"");
+    let result = _try_parse_enclosed_value(&mut parser, '\"');
     assert!(matches!(result, Err(e) if e.downcast_ref::<ParsingError>().unwrap().to_string().contains("Unterminated string")));
     Ok(())
 }

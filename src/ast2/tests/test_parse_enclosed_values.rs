afbfd33dc867759a47ea45adb3cd8c2a8e3278f5
thepyper <thepyper@users.noreply.github.com>
2025-10-21 08:56:58 +0200

    Revert "working"
    This reverts commit e5827becbdbdc80e4e47f6c78b7c98b7c59bb8ae.

--- src/ast2/tests/test_parse_enclosed_values.rs
+++ src/ast2/tests/test_parse_enclosed_values.rs
@@ -28,14 +28,14 @@ fn test_try_parse_enclosed_string_double_quote() {
 fn test_try_parse_enclosed_string_single_quote() {
     let doc = r#"'hello world' rest"#;
     let parser = Parser::new(doc);
-    let p_after_opening_quote = parser.consume_matching_char_immutable('"').unwrap(); // Consume opening quote
+    let p_after_opening_quote = parser.consume_matching_char_immutable('\'').unwrap(); // Consume opening quote
     let (value, p_next) = super::_try_parse_enclosed_string(&p_after_opening_quote, "'").unwrap().unwrap();
     assert_eq!(value, "hello world");
     assert_eq!(p_next.remain(), " rest");
 
     let doc_escaped = r#"'hello \'world\'' rest"#;
     let parser_escaped = Parser::new(doc_escaped);
-    let p_after_opening_quote_escaped = parser_escaped.consume_matching_char_immutable('"').unwrap(); // Consume opening quote
+    let p_after_opening_quote_escaped = parser_escaped.consume_matching_char_immutable('\'').unwrap(); // Consume opening quote
     let (value_escaped, p_next_escaped) = super::_try_parse_enclosed_string(&p_after_opening_quote_escaped, "'").unwrap().unwrap();
     assert_eq!(value_escaped, "hello 'world'"); // Expect unescaped
     assert_eq!(p_next_escaped.remain(), " rest");
@@ -55,7 +55,7 @@ fn test_try_parse_enclosed_value_double_quote() {
 fn test_try_parse_enclosed_value_single_quote() {
     let doc = "'json value' rest";
     let parser = Parser::new(doc);
-    let p_after_opening_quote = parser.consume_matching_char_immutable('"').unwrap(); // Consume opening quote
+    let p_after_opening_quote = parser.consume_matching_char_immutable('\'').unwrap(); // Consume opening quote
     let (value, p_next) = super::_try_parse_enclosed_value(&p_after_opening_quote, "'").unwrap().unwrap();
     assert_eq!(value, json!("json value"));
     assert_eq!(p_next.remain(), " rest");

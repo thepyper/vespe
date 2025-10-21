14b3831fe79b0a6e47fd54272adaa5b143091c6b
thepyper <thepyper@users.noreply.github.com>
2025-10-21 10:00:36 +0200

    Refactor: Use super:: for imports in test_position_range.rs

--- src/ast2/tests/test_position_range.rs
+++ src/ast2/tests/test_position_range.rs
@@ -1,5 +1,5 @@
-use crate::ast2::{Position, Range};
-use crate::ast2::utils::{create_position, create_range};
+use super::{Position, Range};
+use super::utils::{create_position, create_range};
 
 #[test]
 fn test_position_is_valid() {

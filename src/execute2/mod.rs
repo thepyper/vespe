f20ea970c37a3b245637e3e3a3034177b23552a0
thepyper <thepyper@users.noreply.github.com>
2025-10-23 15:00:09 +0200

    working

--- src/execute2/mod.rs
+++ src/execute2/mod.rs
@@ -1,8 +1,10 @@
 mod state;
 mod content;
 mod execute;
+mod variables;
 
 use state::*;
 use content::*;
 
 pub use execute::execute_context;
+pub use execute::collect_context;
---
+++ src/execute2/variables.rs
@@ -0,0 +1,13 @@
+
+struct Variables {
+    provider: String,
+}
+
+impl Variables {
+    pub fn new() -> Self {
+        Variables {
+            provider: "gemini -p -y -m gemini-2.5-flash".to_string(),
+        }
+    }
+
+}

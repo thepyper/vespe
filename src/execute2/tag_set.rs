53fb5d7c4bed952eb4c011ea1f9efe378390f04b
thepyper <thepyper@users.noreply.github.com>
2025-11-06 23:17:40 +0100

    refactor: remove doctest from tag_set.rs

--- src/execute2/tag_set.rs
+++ src/execute2/tag_set.rs
@@ -33,33 +33,6 @@ impl StaticPolicy for SetPolicy {
     /// A `Result` containing the updated [`Collector`] with the new default parameters.
     ///
     /// # Examples
-    ///
-    /// ```rust
-    /// # use crate::execute2::{Collector, Worker, SetPolicy};
-    /// # use crate::execute2::tags::StaticPolicy;
-    /// # use crate::ast2::{Tag, CommandKind, Parameters, Arguments, Range, Position, JsonPlusEntity};
-    /// # use std::sync::Arc;
-    /// # use crate::file::MockFileAccessor;
-    /// # use crate::path::MockPathResolver;
-    /// let worker = Worker::new(Arc::new(MockFileAccessor::new()), Arc::new(MockPathResolver::new()));
-    /// let collector = Collector::new();
-    ///
-    /// let mut params = Parameters::new();
-    /// params.parameters.properties.insert("model".to_string(), JsonPlusEntity::NudeString("gpt-4".to_string()));
-    ///
-    /// let tag = Tag {
-    ///     command: CommandKind::Set,
-    ///     parameters: params,
-    ///     arguments: Arguments::new(),
-    ///     range: Range::null(),
-    /// };
-    ///
-    /// let updated_collector = SetPolicy::collect_static_tag(&worker, collector, &tag).unwrap();
-    /// assert_eq!(
-    ///     updated_collector.default_parameters.parameters.properties.get("model").unwrap(),
-    ///     &JsonPlusEntity::NudeString("gpt-4".to_string())
-    /// );
-    /// ```
     fn collect_static_tag(worker: &Worker, collector: Collector, tag: &Tag) -> Result<Collector> {
         tracing::debug!("tag_set::SetPolicy::collect_static_tag\nTag = {:?}\n", tag);
         Ok(collector.set_default_parameters(&tag.parameters))

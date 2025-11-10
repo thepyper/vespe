d22b520257b1c2e46a9bd39a1ef8f48f00d2af56
thepyper <thepyper@users.noreply.github.com>
2025-11-10 10:37:10 +0100

    log to stderr

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,6 @@ pub mod project;
 pub fn init_telemetry() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
+        .with_writer(std::io::stderr)  
         .init();
 }

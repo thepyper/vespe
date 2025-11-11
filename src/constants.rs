4644524b1c05697553cdccac31588c451b1b72a4
thepyper <thepyper@users.noreply.github.com>
2025-11-11 17:11:44 +0100

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -2,4 +2,3 @@ pub const CTX_DIR_NAME: &str = ".vespe";
 pub const CTX_ROOT_FILE_NAME: &str = ".vespe_root";
 pub const METADATA_DIR_NAME: &str = ".meta";
 pub const CONTEXTS_DIR_NAME: &str = "contexts";
-pub const SNIPPETS_DIR_NAME: &str = "snippets";

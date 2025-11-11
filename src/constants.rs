5b817b713474ad67a5f0b2a06c7976acd9065743
thepyper <thepyper@users.noreply.github.com>
2025-11-11 17:10:05 +0100

    rename .ctx to .vespe

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
-pub const CTX_DIR_NAME: &str = ".ctx";
-pub const CTX_ROOT_FILE_NAME: &str = ".ctx_root";
+pub const CTX_DIR_NAME: &str = ".vespe";
+pub const CTX_ROOT_FILE_NAME: &str = ".vespe_root";
 pub const METADATA_DIR_NAME: &str = ".meta";
 pub const CONTEXTS_DIR_NAME: &str = "contexts";
 pub const SNIPPETS_DIR_NAME: &str = "snippets";

74e8e1fb60821734c52cabbe6689de6a8d943686
thepyper <thepyper@users.noreply.github.com>
2025-11-18 19:35:28 +0100

    refactor(ast2): Update use statements and function calls in test_parse_nude_values.rs

--- src/ast2/tests/test_parse_nude_values.rs
+++ src/ast2/tests/test_parse_nude_values.rs
@@ -1,92 +1,83 @@
-use super::Parser;
+use crate::ast2::parser::Parser;
+use crate::ast2::parser::values;
 use serde_json::json;
 
 #[test]
 fn test_try_parse_nude_integer() {
-    let doc = "123 rest";
-    let parser = Parser::new(doc);
-    let (value, p_next) = super::_try_parse_nude_integer(&parser).unwrap().unwrap();
+    let (value, p_next) = values::_try_parse_nude_integer(&parser).unwrap().unwrap();
     assert_eq!(value, 123);
     assert_eq!(p_next.remain(), " rest");
 
     let doc_no_int = "abc";
     let parser_no_int = Parser::new(doc_no_int);
-    assert!(super::_try_parse_nude_integer(&parser_no_int)
+    assert!(values::_try_parse_nude_integer(&parser_no_int)
         .unwrap()
         .is_none());
 
     let doc_empty = "";
     let parser_empty = Parser::new(doc_empty);
-    assert!(super::_try_parse_nude_integer(&parser_empty)
+    assert!(values::_try_parse_nude_integer(&parser_empty)
         .unwrap()
         .is_none());
 }
 
 #[test]
 fn test_try_parse_nude_float() {
-    let doc = "123.45 rest";
-    let parser = Parser::new(doc);
-    let (value, p_next) = super::_try_parse_nude_float(&parser).unwrap().unwrap();
+    let (value, p_next) = values::_try_parse_nude_float(&parser).unwrap().unwrap();
     assert_eq!(value, 123.45);
     assert_eq!(p_next.remain(), " rest");
 
     let doc_no_float = "123 rest";
     let parser_no_float = Parser::new(doc_no_float);
-    assert!(super::_try_parse_nude_float(&parser_no_float)
+    assert!(values::_try_parse_nude_float(&parser_no_float)
         .unwrap()
         .is_none());
 
     let doc_just_dot = ". rest";
     let parser_just_dot = Parser::new(doc_just_dot);
-    assert!(super::_try_parse_nude_float(&parser_just_dot)
+    assert!(values::_try_parse_nude_float(&parser_just_dot)
         .unwrap()
         .is_none());
 
     let doc_empty = "";
     let parser_empty = Parser::new(doc_empty);
-    assert!(super::_try_parse_nude_float(&parser_empty)
+    assert!(values::_try_parse_nude_float(&parser_empty)
         .unwrap()
         .is_none());
 }
 
 #[test]
 fn test_try_parse_nude_bool() {
-    let doc_true = "true rest";
-    let parser_true = Parser::new(doc_true);
-    let (value_true, p_next_true) = super::_try_parse_nude_bool(&parser_true).unwrap().unwrap();
+    let (value_true, p_next_true) = values::_try_parse_nude_bool(&parser_true).unwrap().unwrap();
     assert_eq!(value_true, true);
     assert_eq!(p_next_true.remain(), " rest");
 
-    let doc_false = "false rest";
-    let parser_false = Parser::new(doc_false);
-    let (value_false, p_next_false) = super::_try_parse_nude_bool(&parser_false).unwrap().unwrap();
+    let (value_false, p_next_false) = values::_try_parse_nude_bool(&parser_false).unwrap().unwrap();
     assert_eq!(value_false, false);
     assert_eq!(p_next_false.remain(), " rest");
 
     let doc_no_bool = "other rest";
     let parser_no_bool = Parser::new(doc_no_bool);
-    assert!(super::_try_parse_nude_bool(&parser_no_bool)
+    assert!(values::_try_parse_nude_bool(&parser_no_bool)
         .unwrap()
         .is_none());
 }
 
 #[test]
 fn test_try_parse_nude_string() {
-    let doc = "hello/world.txt_123 rest";
-    let parser = Parser::new(doc);
-    let (value, p_next) = super::_try_parse_nude_string(&parser).unwrap().unwrap();
+    let (value, p_next) = values::_try_parse_nude_string(&parser).unwrap().unwrap();
     assert_eq!(value, "hello/world.txt_123");
     assert_eq!(p_next.remain(), " rest");
 
     let doc_empty = "";
     let parser_empty = Parser::new(doc_empty);
-    assert!(super::_try_parse_nude_string(&parser_empty)
+    assert!(values::_try_parse_nude_string(&parser_empty)
         .unwrap()
         .is_none());
 
     let doc_with_space = "hello world";
     let parser_with_space = Parser::new(doc_with_space);
-    let (value_space, p_next_space) = super::_try_parse_nude_string(&parser_with_space)
+    let (value_space, p_next_space) = values::_try_parse_nude_string(&parser_with_space)
         .unwrap()
         .unwrap();
     assert_eq!(value_space, "hello");
@@ -95,31 +86,23 @@ fn test_try_parse_nude_string() {
 
 #[test]
 fn test_try_parse_value_nude() {
-    let doc_int = "123 rest";
-    let parser_int = Parser::new(doc_int);
-    let (value_int, p_next_int) = super::_try_parse_value(&parser_int).unwrap().unwrap();
+    let (value_int, p_next_int) = values::_try_parse_value(&parser_int).unwrap().unwrap();
     assert_eq!(value_int, json!(123));
     assert_eq!(p_next_int.remain(), " rest");
 
-    let doc_float = "123.45 rest";
-    let parser_float = Parser::new(doc_float);
-    let (value_float, p_next_float) = super::_try_parse_value(&parser_float).unwrap().unwrap();
+    let (value_float, p_next_float) = values::_try_parse_value(&parser_float).unwrap().unwrap();
     assert_eq!(value_float, json!(123.45));
     assert_eq!(p_next_float.remain(), " rest");
 
-    let doc_bool = "true rest";
-    let parser_bool = Parser::new(doc_bool);
-    let (value_bool, p_next_bool) = super::_try_parse_value(&parser_bool).unwrap().unwrap();
+    let (value_bool, p_next_bool) = values::_try_parse_value(&parser_bool).unwrap().unwrap();
     assert_eq!(value_bool, json!(true));
     assert_eq!(p_next_bool.remain(), " rest");
 
-    let doc_string = "nude_string rest";
-    let parser_string = Parser::new(doc_string);
-    let (value_string, p_next_string) = super::_try_parse_value(&parser_string).unwrap().unwrap();
+    let (value_string, p_next_string) = values::_try_parse_value(&parser_string).unwrap().unwrap();
     assert_eq!(value_string, json!("nude_string"));
     assert_eq!(p_next_string.remain(), " rest");
 
     let doc_empty = "";
     let parser_empty = Parser::new(doc_empty);
-    assert!(super::_try_parse_value(&parser_empty).unwrap().is_none());
+    assert!(values::_try_parse_value(&parser_empty).unwrap().is_none());
 }

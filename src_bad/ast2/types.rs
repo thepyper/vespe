164fc77cb455c43f42eabe46c14a15d2209e3938
thepyper <thepyper@users.noreply.github.com>
2025-11-18 23:36:36 +0100

    cleanup

--- src_bad/ast2/types.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-use std::cmp::Ordering;
-use std::collections::BTreeMap;
-use uuid::{uuid, Uuid};
-
--- src_bad/constants.rs
+++
@@ -1,4 +0,0 @@
-pub const CTX_DIR_NAME: &str = ".vespe";
-pub const CTX_ROOT_FILE_NAME: &str = ".vespe_root";
-pub const METADATA_DIR_NAME: &str = ".meta";
-pub const CONTEXTS_DIR_NAME: &str = "contexts";

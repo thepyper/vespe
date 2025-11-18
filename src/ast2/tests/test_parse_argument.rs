bada519c0a407acf7edf7faa6849f78556ed715d
thepyper <thepyper@users.noreply.github.com>
2025-11-18 19:26:30 +0100

    refactor(ast2): Update use statements and function calls in test_parse_argument.rs

--- src/ast2/tests/test_parse_argument.rs
+++ src/ast2/tests/test_parse_argument.rs
@@ -1,10 +1,9 @@
-use super::*;
+use crate::ast2::parser::Parser;
+use crate::ast2::parser::arguments;
 
 #[test]
 fn test_try_parse_argument_single_quoted() {
-    let doc = "'arg1' rest";
-    let parser = Parser::new(doc);
-    let (arg, p_next) = super::_try_parse_argument(&parser).unwrap().unwrap();
+    let (arg, p_next) = arguments::_try_parse_argument(&parser).unwrap().unwrap();
     assert_eq!(arg.value, "arg1");
     assert_eq!(p_next.remain(), " rest");
     assert_eq!(arg.range.begin.offset, 0);
@@ -13,9 +12,7 @@ fn test_try_parse_argument_single_quoted() {
 
 #[test]
 fn test_try_parse_argument_double_quoted() {
-    let doc = r#""arg2" rest"#;
-    let parser = Parser::new(doc);
-    let (arg, p_next) = super::_try_parse_argument(&parser).unwrap().unwrap();
+    let (arg, p_next) = arguments::_try_parse_argument(&parser).unwrap().unwrap();
     assert_eq!(arg.value, "arg2");
     assert_eq!(p_next.remain(), " rest");
     assert_eq!(arg.range.begin.offset, 0);
@@ -24,9 +21,7 @@ fn test_try_parse_argument_double_quoted() {
 
 #[test]
 fn test_try_parse_argument_nude() {
-    let doc = "nude_arg rest";
-    let parser = Parser::new(doc);
-    let (arg, p_next) = super::_try_parse_argument(&parser).unwrap().unwrap();
+    let (arg, p_next) = arguments::_try_parse_argument(&parser).unwrap().unwrap();
     assert_eq!(arg.value, "nude_arg");
     assert_eq!(p_next.remain(), " rest");
     assert_eq!(arg.range.begin.offset, 0);
@@ -35,16 +30,12 @@ fn test_try_parse_argument_nude() {
 
 #[test]
 fn test_try_parse_argument_empty() {
-    let doc = "";
-    let parser = Parser::new(doc);
-    let result = super::_try_parse_argument(&parser).unwrap();
+    let result = arguments::_try_parse_argument(&parser).unwrap();
     assert!(result.is_none());
 }
 
 #[test]
 fn test_try_parse_argument_no_match() {
-    let doc = "@tag rest";
-    let parser = Parser::new(doc);
-    let result = super::_try_parse_argument(&parser).unwrap();
+    let result = arguments::_try_parse_argument(&parser).unwrap();
     assert!(result.is_none());
 }
